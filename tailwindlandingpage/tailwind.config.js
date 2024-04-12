2c20a89532a9d365177683b86944e6d96ef8142c
Ayush Senapati <ayushsenapati123@gmail.com>
2024-04-12 08:17:57 +0530

    Added google fonts

--- tailwindlandingpage/tailwind.config.js
+++ tailwindlandingpage/tailwind.config.js
@@ -1,8 +1,14 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        playfair: "'Playfair Display', serif",
+        lato: "'Lato', sans-serif",
+      }
+    },
   },
   plugins: [],
 }
