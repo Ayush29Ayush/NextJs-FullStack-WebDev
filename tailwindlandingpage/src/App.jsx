115fe95e64c6ca943d97f1a6b3ce653445d679a6
Ayush Senapati <ayushsenapati123@gmail.com>
2024-04-12 07:43:00 +0530

    Basic layout for components ready

--- tailwindlandingpage/src/App.jsx
+++ tailwindlandingpage/src/App.jsx
@@ -1,12 +1,17 @@
-
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
 
 function App() {
-
   return (
     <>
-      <div>Hello Guys</div>
+      <div className="bg-amber-50 min-h-screen px-6 py-3 flex flex-col gap-16">
+        <Header />
+        <Body />
+        <Footer />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
