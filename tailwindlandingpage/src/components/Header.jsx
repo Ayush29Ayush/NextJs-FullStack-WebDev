d7c64718dd594c58dae3c08896f6201e1f2d2857
Ayush Senapati <ayushsenapati123@gmail.com>
2024-04-12 08:18:15 +0530

    Header basic setup completed

--- tailwindlandingpage/src/components/Header.jsx
+++ tailwindlandingpage/src/components/Header.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { FaBars } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="flex justify-between ">
+      <div className="flex items-center justify-center gap-2">
+        <img src="../../public/assets/Logo.svg" alt="Logo" />
+        <button className="bg-gradient-to-r from-orange-400 to-red-400 text-xs text-white px-2.5 py-1.5 rounded-2xl">Hoster is hiring</button>
+      </div>
+      <div>
+        <FaBars />
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
