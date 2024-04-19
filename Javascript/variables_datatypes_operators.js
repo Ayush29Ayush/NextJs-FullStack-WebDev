7f58d674d15b1518dc141fc21d0f923471c2fb41
Ayush Senapati <ayushsenapati123@gmail.com>
2024-04-19 22:08:15 +0530

    Type conversion and operators added

--- Javascript/variables_datatypes_operators.js
+++ Javascript/variables_datatypes_operators.js
@@ -31,27 +31,81 @@
 // Primitive Datatype
 // 1. Number
 let num = 50;
-console.log(typeof(num));
+console.log(typeof num);
 // 2. String
 let str = "Hello";
-console.log(typeof(str));
+console.log(typeof str);
 // 3. Boolean
 let bool = true;
-console.log(typeof(bool));
+console.log(typeof bool);
 // 4. Null
 let nullVar = null;
-console.log(typeof(nullVar));
+console.log(typeof nullVar);
 // 5. Undefined
 let undef;
-console.log(typeof(undef));
+console.log(typeof undef);
 
 // Non Primitive Datatype
 // 1. Object
-let obj = {name:"Ayush", age:25};
-console.log(typeof(obj));
+let obj = { name: "Ayush", age: 25 };
+console.log(typeof obj);
 // 2. Array
 let arr = [1, 2, 3];
-console.log(typeof(arr));
+console.log(typeof arr);
 // 3. Function
-function func(){};
-console.log(typeof(func));
+function func() {}
+console.log(typeof func);
+
+//! TYPE CONVERSIONS
+let result = "50";
+console.log(result + 10);
+console.log(typeof result);
+result = Number(result);
+console.log(result + 10);
+console.log(typeof result);
+result = String(result);
+console.log(result + 10);
+console.log(typeof result);
+
+let number = 1250.1234;
+number = parseInt(number);
+console.log(number);
+number = parseFloat(number);
+console.log(number);
+
+//! OPERATORS
+console.log(10 + 10);
+console.log(10 - 10);
+console.log(10 * 10);
+console.log(10 / 10);
+console.log(10 % 10);
+console.log(10 ** 10);
+console.log(10 >> 10);
+console.log(10 << 10);
+console.log(10 & 10);
+console.log(10 | 10);
+console.log(10 ^ 10);
+
+let a = 1;
+a++;
+console.log(a);
+++a;
+console.log(a);
+a--;
+console.log(a);
+--a;
+console.log(a);
+
+//! Comparison Operators
+console.log(10 == 10);
+console.log(10 == "10");
+console.log(10 === "10");
+console.log(10 != 10);
+console.log(10 != "10");
+console.log(10 !== "10");
+console.log(10 > 10);
+console.log(10 < 10);
+console.log(10 >= 10);
+console.log(10 <= 10);
+
+console.log(2 > 3 && 2 > 4);
