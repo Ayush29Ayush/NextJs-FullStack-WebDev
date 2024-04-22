2573f782d2294bb00530e8632b1724b771f5f308
Ayush Senapati <ayushsenapati123@gmail.com>
2024-04-22 22:14:39 +0530

    Added DOM onClick

---
+++ Javascript/DOM/onClick.js
@@ -0,0 +1,5 @@
+function changeColor() {
+  //   document.getElementById("myDiv").style.backgroundColor = "green";
+  var myDiv = document.getElementById("myDiv");
+  myDiv.style.backgroundColor = "green";
+}
