791fcedec6390e6e100ff040230de60cfbc99740
Ayush Senapati <ayushsenapati123@gmail.com>
2024-04-23 20:40:13 +0530

    Added DOM styling

---
+++ Javascript/DOM/styling.js
@@ -0,0 +1,7 @@
+document.body.style.backgroundColor = "yellow";
+let a = document.querySelector("h1");
+a.style.color = "red";
+a.style.backgroundColor = "green";
+a.style.textAlign = "center";
+a.style.fontSize = "20px";
+a.style.border = "2px solid black";
