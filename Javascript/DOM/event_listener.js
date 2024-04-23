df0ec34670fa9c76951fb990b355caa33910d0a0
Ayush Senapati <ayushsenapati123@gmail.com>
2024-04-23 22:04:59 +0530

    Added DOM EventListener

---
+++ Javascript/DOM/event_listener.js
@@ -0,0 +1,7 @@
+// let myDiv = document.querySelector("#myDiv");
+// document.getElementById("myDiv").addEventListener("click", function () {
+//     document.getElementById("myDiv").style.backgroundColor = "orange";
+// })
+document.getElementById("myDiv").addEventListener("mouseover", function () {
+  document.getElementById("myDiv").style.backgroundColor = "orange";
+});
