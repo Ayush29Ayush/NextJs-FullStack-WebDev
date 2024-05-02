e4684d474102d96451c3c612e17b457c89cfd734
Ayush Senapati <ayush.senapati@cozentus.com>
2024-05-02 13:41:52 +0530

    useEffect Example added

---
+++ React/03/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import REACT_USE_EFFECT from './useEffect';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <REACT_USE_EFFECT />
+  </React.StrictMode>
+);
---
+++ React/03/src/useEffect.js
@@ -0,0 +1,50 @@
+// eslint-disable-next-line
+import React, { useEffect } from "react";
+
+//! useEffect has a callback function as first parameter and a dependency array as second parameter
+const REACT_USE_EFFECT = () => {
+  const [clicked_button, setClickedButton] = React.useState(null);
+  const [count, setCount] = React.useState(0);
+
+  // useEffect(() => {
+  //   //! code to run after every render, update or every time the dependency changes
+  //   console.log("useEffect");
+  // },[dependency]);
+
+  //! Whenever anything new is rendered, the useEffect will be called
+  // useEffect(() => {
+  //   alert("useEffect");
+  // })
+
+  //! Show alert only when the page is loaded for the first time
+  // useEffect(() => {
+  //   alert("useEffect");
+  // }, []);
+
+  //! Show alert only when clicked_button is changed
+  // useEffect(() => {
+  //   alert(clicked_button);
+  // }, [clicked_button]);
+
+  useEffect(() => {
+    console.log(
+      `You have clicked ${clicked_button} or count was updated to ${count}`
+    );
+  }, [clicked_button, count]);
+
+  return (
+    <div>
+      <button onClick={() => setClickedButton("Say Hello")}>Say Hello</button>
+      <br />
+      <hr />
+      <button onClick={() => setClickedButton("To")}>To</button>
+      <br />
+      <hr />
+      <button onClick={() => setClickedButton("Ayush")}>Ayush</button>
+      <h1>{clicked_button}</h1>
+      <button onClick={() => setCount(count + 1)}>Count: {count}</button>
+    </div>
+  );
+};
+
+export default REACT_USE_EFFECT;
